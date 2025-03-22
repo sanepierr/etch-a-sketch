0102988123d5a9ff20c6ffb73f4efe7e66b9e268
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-22 20:00:42 +0300

    created-container+appendchildren

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const container = document.querySelector(".container");
+const divNo = 16
 
+for (let i = 0; i < divNo; i++){
+    const div = document.createElement("div");
+    div.setAttribute('class', 'grid-div')
+    container.appendChild(div);
+
+}
