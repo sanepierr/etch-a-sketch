a32b51c06e8814d5cf9cec85741a2f76fc6fbaf3
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-23 10:55:51 +0300

    nested+loop+for16X16+grid

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.querySelector(".container");
-const divNo = 16
+const divNo = 16;
 
 for (let i = 0; i < divNo; i++){
-    const div = document.createElement("div");
-    div.setAttribute('class', 'grid-div')
-    container.appendChild(div);
-
+    for (let j = 0; j < divNo; j++){
+        const div = document.createElement("div");
+        div.setAttribute('class', 'gridDiv')
+        container.appendChild(div);
+    }
 }
+const divs = document.getElementsByClassName('gridDiv');
